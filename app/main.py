5f70d9123b89b6b66a2d3566c5170959e5097e2f
Surendra-4 <saisurendra.patthigulla@gmail.com>
2026-07-27 22:01:04 +0530

    feat: add CORS support to FastAPI application

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ This is the first program that will be executed when the server starts.
 """
 
 from fastapi import FastAPI
-from . import models
-from . database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from . routers import post, user, auth, vote
 
 # Creating our FastAPI instance
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
         
 # Separate routes to declutter the main file and improve readability
 app.include_router(post.router)
